a44fd9ee0efdc5b2d44d898c266e76667d9b9a98
Kyle Tsuyemura <kt.fremont@gmail.com>
2017-09-20 15:49:42 -0700

    removed local image references

--- client/components/Banner.jsx
+++ client/components/Banner.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-const BannerImg = require('./images/header.png')
-// import BannerImg from 'images/banner.png';
+
 
 class Banner extends React.Component {
    render() {
